cf6b19aad8f2e8690701e30f43c7e9ffddd98b8e
or-schneider <orschneider@protonmail.com>
2021-10-17 18:30:34 +0300

    Implements fibonnaci algorithm

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-let fibonacciN = 5;
+let fibonacciN = 13;
 
 
 function fibbonaci(n){
+    
+    let sum = 0;
+    
+    if(n == 0){
+        sum = 0;
+        return sum;
+    }
+    if(n == 1){
+        sum = 1;
+        return sum;
+    }
+    let number1 = 1;
+    let number2 = 1;
 
-    let result = 734234;
-    return result;
+    for (let i = 2; i < n; i++) {
+        sum = number1+number2;
+        number1 = number2;
+        number2 = sum;
+    }
+    return sum;
 }
 
-let fibonnachyResult = fibbonaci(fibonacciN);
+let fibonnachiResult = fibbonaci(fibonacciN);
 
 let fibonnaciResultNode = document.getElementById("fibonnaciResult");
-fibonnaciResultNode.innerHTML = `The Fibonacci of ${fibonacciN} is ${fibonnachyResult}`;
+fibonnaciResultNode.innerHTML = `The Fibonacci of ${fibonacciN} is ${fibonnachiResult}`;
