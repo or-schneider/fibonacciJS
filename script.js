9c2deef38b9435db7f36e829d9377e19a88fa2e6
or-schneider <orschneider@protonmail.com>
2021-10-17 18:17:13 +0300

    Creates a function template for fibonnachi series

---
+++ script.js
@@ -0,0 +1,13 @@
+let fibonacciN = 5;
+
+
+function fibbonaci(n){
+
+    let result = 734234;
+    return result;
+}
+
+let fibonnachyResult = fibbonaci(fibonacciN);
+
+let fibonnaciResultNode = document.getElementById("fibonnaciResult");
+fibonnaciResultNode.innerHTML = `The Fibonacci of ${fibonacciN} is ${fibonnachyResult}`;
