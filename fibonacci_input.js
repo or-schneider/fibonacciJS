b0e2aefa4f0b0ba0401902f29467ff409cb4f5ea
or-schneider <orschneider@protonmail.com>
2021-10-18 10:40:38 +0300

    Puts fibonacci input into a form

--- fibonacci_input.js
+++ fibonacci_input.js
@@ -2,8 +2,10 @@ let fibonacciInputNode = document.getElementById("fibonacciInput");
 let fibonacciInputButton = document.getElementById("fibonacciInputButton");
 
 fibonacciInputButton.addEventListener('click',submitFibonacciInput)
-function submitFibonacciInput(){
+function submitFibonacciInput(e){
     let userInput = fibonacciInputNode.value;
     let result = fibonacci(userInput);
     updateFibonacciDisplay(userInput, result);
+
+    e.preventDefault();
 }
