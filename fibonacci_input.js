2b5fa5dcd111dcafbc709a11384b5b178f254ea9
or-schneider <orschneider@protonmail.com>
2021-10-24 17:00:25 +0300

    M5 (F): Implements fibonacci input error message

--- fibonacci_input.js
+++ fibonacci_input.js
@@ -1,3 +1,6 @@
+const fibonacciInputMax = 50;
+const fibonacciInputMin = 1;
+
 let fibonacciInputNode = document.getElementById("fibonacciInput");
 let fibonacciInputButton = document.getElementById("fibonacciInputButton");
 
@@ -7,13 +10,46 @@ function submitFibonacciInput(e){
     e.preventDefault();
 
     let userInput = fibonacciInputNode.value;
-    if(userInput < 1 || userInput>50 || userInput == ""){
-
+    if(userInput == "")
+        return;
+    if (userInput < fibonacciInputMin) {
+        showFibonacciInputBelowMinRangeError(fibonacciInputMin);
         return;
     }
+    if(userInput>fibonacciInputMax){
+        showFibonacciInputAboveMaxRangeError(fibonacciInputMax);
+        return;
+    }
+    clearFibonacciInputError();
     showFibonacciResultSpinner();
     calculateFibonacciAtServerSide(userInput, updateFibonacciDisplay);
 
     // let result = calculateFibonacci(userInput);
     // updateFibonacciDisplay(result);
+}
+
+let fibonacciInputError = document.getElementById("fibonacciInputError");
+function clearFibonacciInputError(){
+    fibonacciInputError.textContent = "";
+    fibonacciInputError.classList.add("invisible");
+    fibonacciInputNode.classList.remove("border-danger");
+
+}
+function showFibonacciInputAboveMaxRangeError(maxNumber){
+    
+    showFibonacciInputError();
+
+    let errorMessage = `Can't be larger than ${maxNumber}`;
+    fibonacciInputError.textContent = errorMessage;
+}
+function showFibonacciInputBelowMinRangeError(minNumber){
+    
+    showFibonacciInputError();
+
+    let errorMessage = `Can't be smaller than ${minNumber}`;
+    fibonacciInputError.textContent = errorMessage;
+}
+function showFibonacciInputError(){
+    fibonacciInputError.classList.remove("invisible");
+    fibonacciInputNode.classList.add("border-danger");
 }
